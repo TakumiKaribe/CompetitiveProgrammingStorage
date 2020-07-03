79a94c32ec2ff76bd6790935da8edecb0ca97b85
makanai5610 <takumi.k.5610@gmail.com>
2020-07-04 00:52:54 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -40,37 +40,48 @@ typedef queue<pll> qpl;
 typedef queue<pi> qpi;
 
 template <typename T>
-void printv(const vector<T>& v) {
-    for (int i = 0; i < v.size(); ++i) {
+void printv(const vector<T> &v)
+{
+    for (int i = 0; i < v.size(); ++i)
+    {
         printf("[%d] %d\n", i, v[i]);
     }
 }
 
 template <typename T>
-void printvv(const vector<T>& vv) {
-    for (int i = 0; i < vv.size(); ++i) {
-        for (int j = 0; j < vv[i].size(); ++j) {
+void printvv(const vector<T> &vv)
+{
+    for (int i = 0; i < vv.size(); ++i)
+    {
+        for (int j = 0; j < vv[i].size(); ++j)
+        {
             printf("[%d][%d] %d\n", i, j, vv[i][j]);
         }
     }
 }
 
 template <typename T, typename U>
-void printp(const pair<T, U>& p) {
+void printp(const pair<T, U> &p)
+{
     cout << p.first << " " << p.second << endl;
 }
 
 template <typename T>
-void printvp(const vector<pair<T, T>>& vp) {
-    for (int i = 0; i < vp.size(); ++i) {
+void printvp(const vector<pair<T, T>> &vp)
+{
+    for (int i = 0; i < vp.size(); ++i)
+    {
         printf("[%d] %d, %d\n", vp[i].first, vp[i].second);
     }
 }
 
 template <typename T>
-void printvvp(const vector<vector<pair<T, T>>>& vvp) {
-    for (int i = 0; i < vvp.size(); ++i) {
-        for (int j = 0; j < vvp[i].size(); ++j) {
+void printvvp(const vector<vector<pair<T, T>>> &vvp)
+{
+    for (int i = 0; i < vvp.size(); ++i)
+    {
+        for (int j = 0; j < vvp[i].size(); ++j)
+        {
             printf("[%d][%d] %d, %d\n", i, j, vvp[i][j].first, vvp[i][j].second);
         }
     }
