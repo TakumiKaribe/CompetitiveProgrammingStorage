e57b9c94199f59196b73727e2b87b030929d3eab
makanai5610 <takumi.k.5610@gmail.com>
2020-08-26 20:36:10 +0900

    arrangement

---
+++ cpp/Library/gcd.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+ll gcd(ll a, ll b)
+{
+    ll _max = max(a, b);
+    ll _min = min(a, b);
+    ll mod = _max % _min;
+    return mod ? gcd(_min, _max % _min) : _min;
+}
