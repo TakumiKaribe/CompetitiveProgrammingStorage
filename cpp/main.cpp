f3459ac15a6adbb238dde48691f6b1139f57fa2c
makanai5610 <takumi.k.5610@gmail.com>
2020-08-27 21:47:16 +0900

    format

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <string>
 #include <algorithm>
-#include <queue>
-#include <utility>
-#include <climits>
 #include <bitset>
+#include <climits>
 #include <cmath>
-#include <map>
-#include <tuple>
 #include <complex>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
 #define MOD 1000000007
 
@@ -39,55 +41,36 @@ typedef queue<int> qi;
 typedef queue<pll> qpl;
 typedef queue<pi> qpi;
 
-template <typename T>
-void printv(const vector<T> &v)
-{
-    for (int i = 0; i < v.size(); ++i)
-    {
-        printf("[%d] %d\n", i, v[i]);
+template <typename T> void printv(const vector<T> &v) {
+  for (int i = 0; i < v.size(); ++i) {
+    printf("[%d] %d\n", i, v[i]);
+  }
+}
+
+template <typename T> void printvv(const vector<T> &vv) {
+  for (int i = 0; i < vv.size(); ++i) {
+    for (int j = 0; j < vv[i].size(); ++j) {
+      printf("[%d][%d] %d\n", i, j, vv[i][j]);
     }
+  }
 }
 
-template <typename T>
-void printvv(const vector<T> &vv)
-{
-    for (int i = 0; i < vv.size(); ++i)
-    {
-        for (int j = 0; j < vv[i].size(); ++j)
-        {
-            printf("[%d][%d] %d\n", i, j, vv[i][j]);
-        }
+template <typename T, typename U> void printp(const pair<T, U> &p) {
+  cout << p.first << " " << p.second << endl;
+}
+
+template <typename T> void printvp(const vector<pair<T, T>> &vp) {
+  for (int i = 0; i < vp.size(); ++i) {
+    printf("[%d] %d, %d\n", vp[i].first, vp[i].second);
+  }
+}
+
+template <typename T> void printvvp(const vector<vector<pair<T, T>>> &vvp) {
+  for (int i = 0; i < vvp.size(); ++i) {
+    for (int j = 0; j < vvp[i].size(); ++j) {
+      printf("[%d][%d] %d, %d\n", i, j, vvp[i][j].first, vvp[i][j].second);
     }
+  }
 }
 
-template <typename T, typename U>
-void printp(const pair<T, U> &p)
-{
-    cout << p.first << " " << p.second << endl;
-}
-
-template <typename T>
-void printvp(const vector<pair<T, T>> &vp)
-{
-    for (int i = 0; i < vp.size(); ++i)
-    {
-        printf("[%d] %d, %d\n", vp[i].first, vp[i].second);
-    }
-}
-
-template <typename T>
-void printvvp(const vector<vector<pair<T, T>>> &vvp)
-{
-    for (int i = 0; i < vvp.size(); ++i)
-    {
-        for (int j = 0; j < vvp[i].size(); ++j)
-        {
-            printf("[%d][%d] %d, %d\n", i, j, vvp[i][j].first, vvp[i][j].second);
-        }
-    }
-}
-
-int main()
-{
-    return 0;
-}
+int main() { return 0; }
